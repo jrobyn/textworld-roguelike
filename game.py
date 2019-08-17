bd2d9c5c589a96bc0c5faadd7e2a5b70f72c8dd7
jaye <54039541+jrobyn@users.noreply.github.com>
2019-08-17 19:00:42 +0100

    initial concept

---
+++ game.py
@@ -0,0 +1,177 @@
+import random
+
+####################
+#fight system
+def fightSystem(Uhr,Uhp,Uat,Uag,Mname,Mhp,Mat,Mag):
+    print()
+    Mhr = Mhp
+    print("YOU | Health: {0}/{1} | Attack: {2} | Agility: {3}".format(Uhr,Uhp,Uat,Uag))
+    print("{0} | Health: {1}/{2} | Attack: {3} | Agility: {4}".format(Mname,Mhr,Mhp,Mat,Mag))
+    print("Fight system not yet implemented.")
+    print("YOU WIN!")
+    bg = (random.randrange(1,5) * 5)
+    print("You won {0} gold.".format(bg))
+    return bg
+
+####################
+#intro
+print("GAME START")
+print("(info)")
+
+#character gen
+name1 = ["Ub","Tal","Fir","En","Kul","Rit","Fas","Mal","Hap","Dor","Ary","Erin","Seran","Celim","Bol","Bor","Moh","Isan","Ael","Ow","Dur"]
+name2 = ["or","undil","illa","ma","end","ex","axe","","o","a","u","ia","illf","elf","etta"]
+races = ["Human","Elf","Dwarf","Beastman","Undead"]
+
+rollChar = False
+while rollChar == False:
+    print()
+    print("Rolling character stats...")
+    name = random.choice(name1) + random.choice(name2)
+    race = random.choice(races)
+    Uhp = 50 + (random.randrange(0,20)) * 5
+    Uat = (random.randrange(3,7)) + (random.randrange(0,2))
+    Uag = (random.randrange(3,10))
+    gp = (random.randrange(0,10) * 10)
+    print("{0} the {1}".format(name,race))
+    print("Hitpoints: {0}".format(Uhp))
+    print("Attack: {0}".format(Uat))
+    print("Agility: {0}".format(Uag))
+    print("{0} starting gold".format(gp))
+    choice = input("Accept these stats? y/ n: ")
+    if choice == "y":
+        rollChar = True
+
+####################
+#main game loop
+game = True
+stage = "Plains"
+Uhr = Uhp
+keys = 0
+undeadLast = False
+
+while game == True:
+    print()
+    print("Current stage: {0}\n{1}/{2}hp | {3} gold | {4} keys".format(stage,Uhr,Uhp,gp,keys))
+    input("Press enter to pick a card.")
+    print("Picking a card...")
+    print()
+    event = random.randrange(0,13)
+    #single frequency events
+    if event == 0:
+        stage = "Plains"
+        print("You enter the {0}.".format(stage))
+    if event == 1:
+        stage = "Desert"
+        print("You enter the {0}.".format(stage))
+    if event == 2:
+        stage = "Swamp"
+        print("You enter the {0}.".format(stage))
+    if event == 3:
+        stage = "Forest"
+        print("You enter the {0}.".format(stage))
+    if event == 4:
+        print("You encounter a Slime Monster!\n")
+        gp += fightSystem(Uhr,Uhp,Uat,Uag,"Slime Monster",30,2,4)
+        undeadLast = False
+    if event == 5:
+        print("You encounter a Skeleton!\n")
+        gp += fightSystem(Uhr,Uhp,Uat,Uag,"Skeleton",30,2,4)
+        undeadLast = True
+    if event == 6:
+        print("You spend the night in a haunted inn.")
+        if undeadLast == True:
+            print("You encounter a Skeleton!\n")
+            gp += gp + fightSystem(Uhr,Uhp,Uat,Uag,"Skeleton",30,2,4)
+            undeadLast = True
+        else:
+            print("The night is thankfully uneventful.")
+    if event == 7:
+        print("You meet a herbalist who offers to restore you to full health for 100 gold.")
+        if gp > 99:
+            choice = input("Accept her offer? y/ n: ")
+            if choice == "y":
+                print("You hand over the coins and are healed to full health.")
+                Uhr = Uhp
+                gp -= 100
+            else:
+                print("You reject her offer and continue your journey.")
+        else:
+            print("You don't have enough money to accept her offer.")
+    if event == 8:
+        print("You meet a travelling merchant who offers to sell you a key for 50 gold.")
+        if gp > 49:
+            choice = input("Accept her offer? y/ n: ")
+            if choice == "y":
+                print("You hand over the coins and are given a key.")
+                keys += 1
+                gp -= 50
+            else:
+                print("You reject her offer and continue your journey.")
+        else:
+            print("You don't have enough money to accept her offer.")
+    if event == 9:
+        print("You end up getting into a brawl with a rowdy villager.")
+        skillCheck = random.randrange(4,6)
+        print("Skill check... Villager attack: {0}.".format(skillCheck))
+        if Uat < skillCheck:
+            if gp > 70:
+                gp -= 70
+                print("The villager knocks you off guard and takes 70 gold pieces.")
+            else:
+                gp = 0
+                Uhr -= 5
+                print("The villager knocks you off guard and takes your gold purse.")
+                print("You lose 5hp.")
+        else:
+            gp += 70
+            print("You win the brawl and he apologetically hands over his coin purse with 70 gold.")
+    if event == 10:
+        print("You find a locked treasure chest.")
+        if keys > 0:
+            choice = input("Open the chest and spend 1 key? y/ n: ")
+            if choice == "y":
+                bg = random.randrange(5,30) * 5
+                print("You spend the key and find {0} gold.".format(bg))
+                keys -= 1
+                gp += bg
+            else:
+                print("You leave this chest and continue your journey.")
+        else:
+            print("You don't have any keys to open the chest with.")
+    if event == 11:
+        print("You find a trapped chest.")
+        choice = input("Attempt to open the chest and dodge the trap? y/ n: ")
+        if choice == "y":
+            skillCheck = random.randrange(1,12)
+            print("Skill check... Trap level: {0}.".format(skillCheck))
+            if Uag < skillCheck:
+                bg = random.randrange(5,15) * 5
+                print("You successfully avoid the trap and find {0} gold!".format(bg))
+                gp += bg
+            else:
+                damage = skillCheck * 2
+                print("You fail to avoid the trap and lose {0}hp.".format(damage))
+                Uhr -= damage
+        else:
+            print("You leave this chest and continue your journey.")
+    if event == 12:
+        if race == "Elf":
+            print("You meet some fellow elves who offer to restore your health points for free.")
+            print("Health restored to full.")
+            Uhr = Uhp
+        else:
+            print("You meet some elves, but they don't want much to do with outsiders.")
+
+    if event == 13:
+        print("You meet a gambler.")
+    if event == 14:
+        print("You meet a gambler.")
+        
+    #dual frequency events
+    ##NONE##
+
+    #check if dead
+    if Uhr < 1:
+        print("You have no more healthpoints left.\nGAME OVER")
+        game = False
